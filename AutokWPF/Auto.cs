ac2b810bee269820eaa2caf22c1e90d7c964a489
ddaniel-bit <danko8651@gmail.com>
2025-04-04 09:12:09 +0200

    wpf kesz

---
+++ AutokWPF/Auto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Autok
+{
+    class Auto
+    {
+        int sorszam;
+        string marka;
+        string model;
+        int gyartasi_ev;
+        string szin;
+        int eladott_darabszam;
+        double atlagos_eladasi_ar;
+
+        public Auto(string sor)
+        {
+            string[] splitelve = sor.Split(";");
+            this.Sorszam = int.Parse(splitelve[0]);
+            this.Marka = splitelve[1];
+            this.Model = splitelve[2];
+            this.Gyartasi_ev = int.Parse(splitelve[3]);
+            this.Szin = splitelve[4];
+            this.Eladott_darabszam = int.Parse(splitelve[5]);
+            this.Atlagos_eladasi_ar = double.Parse(splitelve[6]);
+        }
+
+        public int Sorszam { get => sorszam; set => sorszam = value; }
+        public string Marka { get => marka; set => marka = value; }
+        public string Model { get => model; set => model = value; }
+        public int Gyartasi_ev { get => gyartasi_ev; set => gyartasi_ev = value; }
+        public string Szin { get => szin; set => szin = value; }
+        public int Eladott_darabszam { get => eladott_darabszam; set => eladott_darabszam = value; }
+        public double Atlagos_eladasi_ar { get => atlagos_eladasi_ar; set => atlagos_eladasi_ar = value; }
+        public static List<Auto> Beolvasas(string eleresi_ut)
+        {
+            List<Auto> autoklistaba = new List<Auto>();
+            StreamReader sr = new StreamReader(eleresi_ut);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                autoklistaba.Add(new Auto(sr.ReadLine()));
+            }
+            return autoklistaba;
+        }
+    }
+}
---
+++ AutokWPF/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using Autok;
+
+namespace AutokWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    List<Auto> autok;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Betolt_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog megnyitas = new OpenFileDialog();
+        megnyitas.ShowDialog();
+        string path = megnyitas.FileName;
+        if (path != "")
+        {
+            autok = Auto.Beolvasas(path);
+        }
+        dataGridAutok.ItemsSource = autok;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        int beirtev;
+        List<string> markamodel = new List<string>();
+        try
+        {
+            beirtev = int.Parse(txtGyartasiEv.Text);
+            List<Auto> szurt = autok.Where(x => x.Gyartasi_ev == beirtev).ToList();
+            foreach (var item in szurt)
+            {
+                markamodel.Add($"{item.Marka} {item.Model}");
+            }
+        }
+        catch (Exception)
+        {
+
+            MessageBox.Show("nem jo formatum");
+        }
+        listBoxAutok.ItemsSource = markamodel;
+    }
+
+    private void Bezar_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBoxResult result = MessageBox.Show("Biztosan ki szeretne lépni?",
+                                                  "Kilépés megerősítése",
+                                                  MessageBoxButton.YesNo,
+                                                  MessageBoxImage.Question);
+
+        if (result == MessageBoxResult.Yes)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
